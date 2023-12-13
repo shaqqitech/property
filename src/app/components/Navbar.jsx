4fe39063e4d311bd1b9a7adbdb16991d7f441a45
Mehar Ali <mehar0alii@gmail.com>
2023-12-13 13:26:23 +0500

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <main className='w-20 h-screen fixed top-0 left-0 border-2 bg-slate-200 p-3'>
+        
+    </main>
+  )
+}
+
+export default Navbar
