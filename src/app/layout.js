67f100cf984c661445ec4d512ffc088b24d3d0ad
Mehar Ali <mehar0alii@gmail.com>
2023-12-27 11:17:50 +0500

    make the navbar position fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,23 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'A Properties App',
-  description: 'A properties buy and rent app',
-}
+  title: "A Properties App",
+  description: "A properties buy and rent app",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} scrollbar-hide`}>
+      <body className={`${inter.className} scrollbar-hide flex justify-between`}>
+        <div className="fixed">
+          <Navbar />
+        </div>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 import Content from "./components/Content";
 import Filter from "./components/Filter";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="max-w-screen min-h-screen flex">
       <div className="w-[10vw] lg:w-[5vw]">
-        <Navbar />
+        {/* <Navbar /> */}
       </div>
       <div className="w-[90vw] lg:w-[95vw] flex flex-col">
         <Header />
